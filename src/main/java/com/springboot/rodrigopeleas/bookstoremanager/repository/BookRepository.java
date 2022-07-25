d02b82895c98cdb8802eedd4fe136582fa3c04aa
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-07-25 19:48:33 -0300

    class 18 Created book repository class

---
+++ src/main/java/com/springboot/rodrigopeleas/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.rodrigopeleas.bookstoremanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.rodrigopeleas.bookstoremanager.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
