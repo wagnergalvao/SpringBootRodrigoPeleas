779b2f884746a92f7c132070c3da3bd5925277ad
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-07-25 16:21:11 -0300

    class 16 create entity Author

---
+++ src/main/java/com/springboot/rodrigopeleas/bookstoremanager/entity/Author.java
@@ -0,0 +1,21 @@
+package com.springboot.rodrigopeleas.bookstoremanager.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true, length = 100)
+  private String name;
+
+  @Column(nullable = false, length = 2)
+  private Integer age;
+}
