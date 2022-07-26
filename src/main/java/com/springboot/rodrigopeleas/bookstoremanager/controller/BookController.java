c95d381c43ec348040721a25a43df64f273e2995
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-07-26 18:54:01 -0300

    refactored Controller and Service to use BookDTO

--- src/main/java/com/springboot/rodrigopeleas/bookstoremanager/controller/BookController.java
+++ src/main/java/com/springboot/rodrigopeleas/bookstoremanager/controller/BookController.java
@@ -1,12 +1,14 @@
 package com.springboot.rodrigopeleas.bookstoremanager.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.rodrigopeleas.bookstoremanager.dto.BookDTO;
 import com.springboot.rodrigopeleas.bookstoremanager.dto.MessageResponseDTO;
-import com.springboot.rodrigopeleas.bookstoremanager.entity.Book;
 import com.springboot.rodrigopeleas.bookstoremanager.service.BookService;
 
 @RestController
@@ -20,8 +22,8 @@ public class BookController {
   }
 
   @PostMapping
-  public MessageResponseDTO create(@RequestBody Book book) {
-    return bookService.create(book);
+  public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
+    return bookService.create(bookDTO);
   }
 
 }
--- src/main/java/com/springboot/rodrigopeleas/bookstoremanager/service/BookService.java
+++ src/main/java/com/springboot/rodrigopeleas/bookstoremanager/service/BookService.java
@@ -2,6 +2,7 @@ package com.springboot.rodrigopeleas.bookstoremanager.service;
 
 import org.springframework.stereotype.Service;
 
+import com.springboot.rodrigopeleas.bookstoremanager.dto.BookDTO;
 import com.springboot.rodrigopeleas.bookstoremanager.dto.MessageResponseDTO;
 import com.springboot.rodrigopeleas.bookstoremanager.entity.Book;
 import com.springboot.rodrigopeleas.bookstoremanager.repository.BookRepository;
@@ -15,9 +16,17 @@ public class BookService {
     this.bookRepository = bookRepository;
   }
 
-  public MessageResponseDTO create(Book book) {
+  public MessageResponseDTO create(BookDTO bookDTO) {
 
-    Book savedBook = bookRepository.save(book);
+    Book bookToSave = Book.builder()
+        .name(bookDTO.getName())
+        .pages(bookDTO.getPages())
+        .chapters(bookDTO.getChapters())
+        .isbn(bookDTO.getIsbn())
+        .publisherName(bookDTO.getPublisherName())
+        .build();
+
+    Book savedBook = bookRepository.save(bookToSave);
     return MessageResponseDTO.builder()
         .message("Book created with ID " + savedBook.getId())
         .build();
