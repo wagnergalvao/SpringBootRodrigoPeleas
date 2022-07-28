e5fd2405cce284fe94d56aec22730c07953ac31c
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-07-27 23:17:14 -0300

    class 23 developed unit tests for controller

--- src/test/java/com/springboot/rodrigopeleias/bookstoremanager/controller/BookControllerTest.java
+++ src/test/java/com/springboot/rodrigopeleias/bookstoremanager/controller/BookControllerTest.java
@@ -1,17 +1,92 @@
 package com.springboot.rodrigopeleias.bookstoremanager.controller;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import static com.springboot.rodrigopeleias.bookstoremanager.utils.BookUtils.asJsonString;
+import static com.springboot.rodrigopeleias.bookstoremanager.utils.BookUtils.createFakeBookDTO;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import com.springboot.rodrigopeleias.bookstoremanager.dto.BookDTO;
+import com.springboot.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
 import com.springboot.rodrigopeleias.bookstoremanager.service.BookService;
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
 
+  /**
+   *
+   */
+  private static final String BOOK_API_URL_PATH = "/api/v1/books";
+
   private MockMvc mockMvc;
 
+  @Mock
   private BookService BookService;
 
+  @InjectMocks
   private BookController bookController;
+
+  @BeforeEach
+  void setUp() {
+    mockMvc = MockMvcBuilders.standaloneSetup(bookController)
+        .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+        .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+        .build();
+  }
+
+  @Test
+  void WhenPostCreateIsCalledWithBookDataThenBookShouldBeCreated() throws Exception {
+
+    BookDTO bookDTO = createFakeBookDTO();
+    MessageResponseDTO expectMessageResponseDTO = MessageResponseDTO.builder()
+        .message("Book created with ID " + bookDTO.getId())
+        .build();
+
+    when(BookService.create(bookDTO))
+        .thenReturn(expectMessageResponseDTO);
+
+    mockMvc.perform(post(BOOK_API_URL_PATH)
+        .contentType(APPLICATION_JSON)
+        .content(asJsonString(bookDTO)))
+        .andExpect(status().isOk())
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.message",
+                Is.is(expectMessageResponseDTO.getMessage())));
+  }
+
+  @Test
+  void WhenPostCreateIsCalledWithInvalidSIBNThenBadRequestShouldBeReturned() throws Exception {
+    BookDTO bookDTO = createFakeBookDTO();
+    bookDTO.setIsbn("invalid isbn");
+
+    mockMvc.perform(post(BOOK_API_URL_PATH)
+        .contentType(APPLICATION_JSON)
+        .content(asJsonString(bookDTO)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void WhenPostCreateIsCalledWithUnnamedBookhenBadRequestShouldBeReturned() throws Exception {
+    BookDTO bookDTO = createFakeBookDTO();
+    bookDTO.setName("");
+
+    mockMvc.perform(post(BOOK_API_URL_PATH)
+        .contentType(APPLICATION_JSON)
+        .content(asJsonString(bookDTO)))
+        .andExpect(status().isBadRequest());
+  }
 }
---
+++ src/test/java/com/springboot/rodrigopeleias/bookstoremanager/utils/BookUtils.java
@@ -0,0 +1,68 @@
+package com.springboot.rodrigopeleias.bookstoremanager.utils;
+
+import static com.springboot.rodrigopeleias.bookstoremanager.utils.AuthorUtils.createFakeAuthor;
+import static com.springboot.rodrigopeleias.bookstoremanager.utils.AuthorUtils.createFakeAuthorDTO;
+import static com.springboot.rodrigopeleias.bookstoremanager.utils.AuthorUtils.createFakeAuthorFrom;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.javafaker.Faker;
+import com.springboot.rodrigopeleias.bookstoremanager.dto.BookDTO;
+import com.springboot.rodrigopeleias.bookstoremanager.entity.Book;
+
+public class BookUtils {
+
+  private static final Faker faker = Faker.instance();
+
+  public static BookDTO createFakeBookDTO() {
+    return BookDTO.builder()
+        .id(faker.number().randomNumber())
+        .name(faker.book().title())
+        .pages(faker.number().numberBetween(0, 200))
+        .chapters(faker.number().numberBetween(1, 20))
+        .isbn("0-596-52068-9")
+        .publisherName(faker.book().publisher())
+        .author(createFakeAuthorDTO())
+        .build();
+  }
+
+  public static Book createFakeBook() {
+    return Book.builder()
+        .id(faker.number().randomNumber())
+        .name(faker.book().title())
+        .pages(faker.number().numberBetween(0, 200))
+        .chapters(faker.number().numberBetween(1, 20))
+        .isbn("0-596-52068-9")
+        .publisherName(faker.book().publisher())
+        .author(createFakeAuthor())
+        .build();
+  }
+
+  public static Book createFakeBookFrom(BookDTO bookDTO) {
+    return Book.builder()
+        .id(bookDTO.getId())
+        .name(bookDTO.getName())
+        .pages(bookDTO.getPages())
+        .chapters(bookDTO.getChapters())
+        .isbn(bookDTO.getIsbn())
+        .publisherName(bookDTO.getPublisherName())
+        .author(createFakeAuthorFrom(bookDTO.getAuthor()))
+        .build();
+  }
+
+  public static String asJsonString(BookDTO bookDTO) {
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+      objectMapper.registerModules(new JavaTimeModule());
+
+      return objectMapper.writeValueAsString(bookDTO);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
